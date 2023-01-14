4c911e26cd174769fe7c605c534b33dd1da50246
ray-chunkit-chung <ray.chunkit.chung@gmail.com>
2023-01-14 08:47:04 +0000

    add var in hello world

--- HelloWorld/ShowCase/Program.cs
+++ HelloWorld/ShowCase/Program.cs
@@ -1,2 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
+
+var name = Console.ReadLine();
+
 Console.WriteLine("Hello, World!");
+Console.WriteLine("Hello, World!2");
+Console.WriteLine($"Hello, World!3. {name}");
+Console.WriteLine("Hello, World!4");
