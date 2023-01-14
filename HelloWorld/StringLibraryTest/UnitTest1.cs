4abf0c534c8df52e7d9dce055e7359bf45c8e3d2
ray-chunkit-chung <ray.chunkit.chung@gmail.com>
2023-01-14 12:13:54 +0000

    Add unit test

--- HelloWorld/StringLibraryTest/UnitTest1.cs
+++ HelloWorld/StringLibraryTest/UnitTest1.cs
@@ -1,10 +1,34 @@
+using StringLibrary;
+
 namespace StringLibraryTest;
 
 [TestClass]
 public class UnitTest1
 {
-    [TestMethod]
-    public void TestMethod1()
+  [TestMethod]
+  public void TestStartsWithUpper()
+  {
+    string[] words = { "A", "B", "C" };
+    foreach (string word in words)
     {
+      bool result = word.StartsWithUpper();
+      Assert.IsTrue(
+        result,
+        string.Format("Expected for '{0}': true; Actual: {1}", word, result)
+      );
     }
+  }
+  [TestMethod]
+  public void TestInteger()
+  {
+    string[] words = { "1", "2", "3", null };
+    foreach (string word in words)
+    {
+      bool result = word.StartsWithUpper();
+      Assert.IsFalse(
+        result,
+        string.Format("Expected for '{0}': false; Actual: {1}", word, result)
+      );
+    }
+  }
 }
